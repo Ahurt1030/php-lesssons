be9f850e4f5304a6ef6b291bf82994189785dbaa
Ahurt1030 <141744687+Ahurt1030@users.noreply.github.com>
2025-03-28 09:07:46 -0500

    Create datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Datatypes</title>
+</head>
+<body>
+    
+<?php
+//String
+$name = 'I can code!';
+
+//Integer
+$name = 28;
+
+//Float
+$name = 37.84789;
+
+//Boolean
+
+
+//Array
+$names = array('Austin', 'Logan', 'Franklin');
+echo $names[2];
+
+?>
+</body>
+</html>
