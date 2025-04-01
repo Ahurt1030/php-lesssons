7f81d1cfc790119e8f240b40cc11734b990a361c
Ahurt1030 <141744687+Ahurt1030@users.noreply.github.com>
2025-04-01 09:42:51 -0500

    gf

---
+++ calender.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>My Web Page</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<form>
+
+</form>
+
+<?php
+    $dayofweek = date("w");
+    
+    switch ($dayofweek) {
+        case 1:
+            echo "<p>It is Monday!</p>";
+            break;
+        case 2:
+            echo "<p>It is Tuesday!</p>";
+            break;
+        case 3:
+            echo "<p>It is Wednesday!</p>";
+            break;
+        case 4:
+            echo "<p>It is Thursday!</p>";
+            break;
+        case 5:
+            echo "<p>It is Friday!</p>";
+            break;
+        case 6:
+            echo "<p>It is Saturday!</p>";
+            break;
+        case 0:
+            echo "<p>It is Sunday!</p>";
+            break;
+
+    }
+?>
+
+</body>
+</html>
