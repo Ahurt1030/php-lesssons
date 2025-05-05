3b907d3614ce1c027cd367fa1f03d35deec33be7
Ahurt1030 <141744687+Ahurt1030@users.noreply.github.com>
2025-05-05 09:17:47 -0500

    hgdf

--- superglobals.php
+++ superglobals.php
@@ -3,19 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookie Expiry Example</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Cookie Expiry Example with PHP</h1>
-    <p>
-        <?php
-        if (isset($_COOKIE[$cookie_name])) {
-            echo "Cookie '{$cookie_name}' is set! Value: " . $_COOKIE[$cookie_name];
-        } else {
-            echo "Cookie '{$cookie_name}' has not been set yet. Please reload the page.";
-        }
-        ?>
-    </p>
-    <p>The cookie will expire in 2 days from now.</p>
+    <?php
+    include "includes/header.php";
+    setcookie("Name","Omar", time() + 86400);
+
+    $_SESSION['name']= '12';
+
+    ?>
 </body>
 </html>
